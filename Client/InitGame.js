2ab858171bd1d5cd7e45d336ccab1da4bc56d361
xutao <429499381@qq.com>
2017-12-20 10:04:49 +0800

    初始化OK， 开始方块渲染

--- Client/InitGame.js
+++ Client/InitGame.js
@@ -1,58 +1,67 @@
 // 初始数据模版
-var initData = function (gameId) {
+var initData = function (gameId, gameId1) {
     // 游戏主体数据模版 10*20
     var gameData = [
 
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     ];
     // 下一步数据模版 4*4
     var nextData = [
-        [0,0,0,0],
-        [0,0,0,0],
-        [0,0,0,0]
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
     ];
 
     // 传递进来的游戏主体 div
     var gameDiv = document.getElementById(gameId);
-    // 准备要插入的 小方块 20*20
-    var square = document.createElement('div');
-    square.className = 'none';
-    square.style.width = 20 + 'px';
-    square.style.height = 20 + 'px';
-    square.style.backgroundColor = 'red';
-    square.position = 'absolute';
+    var nextDiv = document.getElementById(gameId1);
     var gameDivs = [];
     var nextDivs = [];
 
-    for(var i = 0; i < gameData.length; i++) {
-        for(var j = 0; j < gameData[i].length; j++) {
-                // 在主体游戏框架中填充 20*20 的 默认色方块 （白色）
-                square.style.top = i * 20 + 'px';
-                square.style.left = j * 20 + 'px';
-                gameDiv.appendChild(square);
+    var squareData = function (datas, dataDivs, domDiv) {
+        for (var i = 0; i < datas.length; i++) {
+            console.log(datas.length);
+            var data = []
+            for (var j = 0; j < datas[0].length; j++) {
+                // 在主体游戏框架中填充 20*20 的 默认色方块（白色）
+                var square = document.createElement('div');
+                square.className = 'none';
+                square.style.width = 20 + 'px';
+                square.style.height = 20 + 'px';
+                square.style.backgroundColor = '#ff8484';
+                square.style.position = 'absolute';
+                square.style.top = (i * 20) + 'px';
+                square.style.left = (j * 20) + 'px';
+                domDiv.appendChild(square);
+                data.push(square);
+            }
+            dataDivs.push(data);
         }
-    }
+    };
+    squareData(gameData, gameDivs, gameDiv);
+    squareData(nextData, nextDivs, nextDiv);
 
-    console.log('插入完毕'  )
+    console.log('插入完毕')
 }
 
-initData('game');
+initData('game', 'next');
