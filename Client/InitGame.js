f1e907ba130dd9546ffbee79e2ae4f1422701ed3
csdn <429499381@qq.com>
2017-12-23 12:44:17 +0800

    初始化OK， 开始方块渲染  报错不能 读取 0

---
+++ Client/InitGame.js
@@ -0,0 +1,241 @@
+// 传递进来的游戏主体 div
+var gameDiv = document.getElementById('game');
+var nextDiv = document.getElementById('next');
+var gameDivs = [];
+var nextDivs = [];
+
+// 方块起始点
+var origin = {
+    x: 0,
+    y: 4
+};
+
+// 游戏主体数据模版 10*20
+var gameData = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
+
+// 下一步数据模版 4*4
+var nextData = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+];
+
+// 初始数据模版
+var initData = function () {
+
+    var squareData = function (datas, dataDivs, domDiv) {
+        for (var i = 0; i < datas.length; i++) {
+            var data = []
+            for (var j = 0; j < datas[0].length; j++) {
+                // 在主体游戏框架中填充 20*20 的 默认色方块（白色）
+                var square = document.createElement('div');
+                square.className = 'none';
+                square.style.width = 20 + 'px';
+                square.style.height = 20 + 'px';
+                square.style.position = 'absolute';
+                square.style.top = (i * 20) + 'px';
+                square.style.left = (j * 20) + 'px';
+                domDiv.appendChild(square);
+                data.push(square);
+            }
+            dataDivs.push(data);
+        }
+    };
+    squareData(gameData, gameDivs, gameDiv);
+    squareData(nextData, nextDivs, nextDiv);
+}
+
+// 七种方块模版
+var squareRandom = function () {
+    // 获取随机 方向 方块形状
+    var dirNum = Math.ceil(Math.random() * 4) - 1;
+    var squareNum = Math.ceil(Math.random() * 5) - 1;
+
+    // 方块 数据模版
+    var squareData0 = [
+        [
+            [1, 1, 1, 1],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [1, 0, 0, 0],
+            [1, 0, 0, 0],
+            [1, 0, 0, 0],
+            [1, 0, 0, 0]
+        ],
+        [
+            [1, 1, 1, 1],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [1, 0, 0, 0],
+            [1, 0, 0, 0],
+            [1, 0, 0, 0],
+            [1, 0, 0, 0]
+        ]
+    ]; // 1
+    var squareData1 = [ // 7
+        [
+            [1, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 1, 0, 0]
+        ],
+        [
+            [1, 1, 0, 0],
+            [1, 0, 0, 0],
+            [1, 0, 0, 0],
+            [1, 0, 0, 0]
+        ],
+        [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [1, 0, 0, 0],
+            [1, 1, 1, 0]
+        ],
+        [
+            [0, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+            [1, 1, 0, 0]
+        ]
+    ]; // 7
+    var squareData2 = [
+        [
+            [0, 0, 0, 0],
+            [0, 1, 1, 0],
+            [0, 1, 1, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [0, 0, 0, 0],
+            [0, 1, 1, 0],
+            [0, 1, 1, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [0, 0, 0, 0],
+            [0, 1, 1, 0],
+            [0, 1, 1, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [0, 0, 0, 0],
+            [0, 1, 1, 0],
+            [0, 1, 1, 0],
+            [0, 0, 0, 0]
+        ]
+    ]; // 田
+    var squareData3 = [
+        [
+            [0, 1, 0, 0],
+            [1, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [1, 0, 0, 0],
+            [1, 1, 0, 0],
+            [1, 0, 0, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [0, 1, 0, 0],
+            [1, 1, 1, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [1, 1, 1, 0],
+            [0, 1, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ]
+    ]; // 上
+    var squareData4 = [
+        [
+            [1, 0, 0, 0],
+            [1, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [0, 1, 0, 0],
+            [1, 1, 0, 0],
+            [1, 0, 0, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [1, 1, 0, 0],
+            [0, 1, 1, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [0, 1, 1, 0],
+            [1, 1, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ]
+    ]; // z
+
+    var backSquare = function () {
+        if (squareNum === 0) {
+            return squareData0[dirNum];
+        } else if (squareNum === 1) {
+            return squareData1[dirNum]
+        } else if (squareNum === 2) {
+            return squareData2[dirNum]
+        } else if (squareNum === 3) {
+            return squareData3[dirNum]
+        } else if (squareNum === 4) {
+            return squareData4[dirNum]
+        }
+        return false;
+    };
+    backSquare();
+    console.log(backSquare());
+};
+var square = squareRandom();
+
+// 设置数据
+var setData = function () {
+    for(var x = 0; x < nextData[0].length; x++) {
+        for(var y = 0; y < nextData.length; j++) {
+            console.log(square[x][y]);
+            if(square[x][y] !== 0) {
+                nextDivs[x][y] = square[x][y];
+            }
+        }
+    }
+};
+
+
+initData();
+setData();
