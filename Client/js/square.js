ae06ac9e3310b98bb4f9fd2dd37918ee69bf5811
csdn <429499381@qq.com>
2018-01-11 21:28:51 +0800

    合并

--- Client/js/square.js
+++ Client/js/square.js
@@ -1,95 +1,205 @@
-var Square = function () {
-    // 方块数据
-    this.data = [
-        [1, 0, 0, 0],
-        [1, 0, 0, 0],
-        [1, 0, 0, 0],
-        [1, 0, 0, 0]
-    ];
+// 五种方块模版
+var Square = function (type, dir) {
+    // 获取随机方向方块形状
+    var dirNum;
+    var squareNum;
+    dirNum = Math.ceil(Math.random() * 4) - 1;
+    squareNum = Math.ceil(Math.random() * 5) - 1;
 
-    // 原点
+    var that = this;
     this.origin = {
         x: 0,
-        y: 0
+        y: 4,
+        dir: null,
+        squareNum: null
     };
 
-    // 方向
-    this.dir = 0;
+    if (type >= 0 && dir >= 0) {
+        that.origin.dir = dir;
+        that.origin.squareNum = type;
 
-    // 旋转数组
-    this.rotates = [
-        [
-            [0,1,0,0],
-            [0,1,0,0],
-            [0,1,0,0],
-            [0,1,0,0]
-        ],
-        [
-            [0,0,0,0],
-            [1,1,1,1],
-            [0,0,0,0],
-            [0,0,0,0]
-        ],
-        [
-            [0,1,0,0],
-            [0,1,0,0],
-            [0,1,0,0],
-            [0,1,0,0]
-        ],
-        [
-            [0,0,0,0],
-            [1,1,1,1],
-            [0,0,0,0],
-            [0,0,0,0]
-        ]
-    ];
-}
+    } else {
+        that.origin.dir = dirNum;
+        that.origin.squareNum = squareNum;
 
-Square.prototype.canRotate = function (isValid) {
-    var test = {};
-    this.dir = this.dir + 1;
-    if(this.dir === 4) {
-        this.dir = 0;
     }
-    for(var i = 0; i < this.data.length; i++) {
-        for(var j = 0; j < this.data[0].length; j++) {
-            this.data[i][j] = this.rotates[this.dir][i][j];
+
+
+    this.data = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ]
+
+    // 方块 数据模版
+    var squareData0 = [
+        [
+            [1, 1, 1, 1],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [1, 0, 0, 0],
+            [1, 0, 0, 0],
+            [1, 0, 0, 0],
+            [1, 0, 0, 0]
+        ],
+        [
+            [1, 1, 1, 1],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [1, 0, 0, 0],
+            [1, 0, 0, 0],
+            [1, 0, 0, 0],
+            [1, 0, 0, 0]
+        ]
+    ]; // 1
+    var squareData1 = [ // 7
+        [
+            [1, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [1, 1, 0, 0],
+            [1, 0, 0, 0],
+            [1, 0, 0, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [1, 0, 0, 0],
+            [1, 1, 1, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [1, 1, 1, 0],
+            [1, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ]
+    ]; // 7
+    var squareData2 = [
+        [
+            [1, 1, 0, 0],
+            [1, 1, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [1, 1, 0, 0],
+            [1, 1, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [1, 1, 0, 0],
+            [1, 1, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [1, 1, 0, 0],
+            [1, 1, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ]
+    ]; // 田
+    var squareData3 = [
+        [
+            [0, 1, 0, 0],
+            [1, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [1, 0, 0, 0],
+            [1, 1, 0, 0],
+            [1, 0, 0, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [0, 1, 0, 0],
+            [1, 1, 1, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [1, 1, 1, 0],
+            [0, 1, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ]
+    ]; // 上
+    var squareData4 = [
+        [
+            [1, 0, 0, 0],
+            [1, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [0, 1, 0, 0],
+            [1, 1, 0, 0],
+            [1, 0, 0, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [1, 1, 0, 0],
+            [0, 1, 1, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ],
+        [
+            [0, 1, 1, 0],
+            [1, 1, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ]
+    ]; // z
+
+    this.getSquare = function (type, dir) {
+        var data = [];
+        if (type >= 0 && dir >= 0) {
+            data = eval('squareData' + type)[dir];
+            return data
         }
     }
-    test.x = this.origin.x + 1;
-    test.y = this.origin.y;
-    return isValid(test, this.data);
-}
-Square.prototype.rotate = function () {
-    this.origin.x = this.origin.x + 1;
-}
 
-Square.prototype.canDown = function (isValid) {
-    var test = {};
-    test.x = this.origin.x + 1;
-    this.y = this.origin.y;
-    return isValid(test, this.data);
-}
-Square.prototype.down = function () {
-    this.origin.x = this.origin.x + 1;
-}
+    this.createData = function () {
+        that.data = that.getSquare(that.origin.squareNum, that.origin.dir);
+    }
 
-Square.prototype.canLeft = function (isValid) {
-    var test = {};
-    test.x = this.origin.x;
-    test.y = this.origin.y - 1;
-    return isValid(test, this.data);
-}
-Square.prototype.left = function () {
-    this.origin.y = this.origin.y - 1;
-}
 
-Square.prototype.canRight = function (isValid) {
-    var test = {};
-    test.x = this.origin.x;
-    test.y = this.origin.y + 1;
-    return isValid(test, this.data);
-}
-Square.prototype.right = function () {
-    this.origin.y = this.origin.y + 1;
-}
+    this.down = function () {
+        this.origin.x++;
+    }
+    this.left = function () {
+        this.origin.y--;
+    }
+    this.right = function () {
+        this.origin.y++;
+    }
+    this.rotate = function () {
+        if (this.origin.dir + 1 > squareData0.length - 1) {
+            this.origin.dir = 0;
+        } else {
+            this.origin.dir++;
+        }
+        this.data = this.getSquare(this.origin.squareNum, this.origin.dir);
+    }
+
+    // 初始化执行一次
+    this.createData();
+};
+
+
+
+
+
